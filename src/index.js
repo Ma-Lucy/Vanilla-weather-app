d31b9ad01fb896c0f144338f71d1c8df6a49f120
Ma-Lucy <161073044+Ma-Lucy@users.noreply.github.com>
2024-03-19 09:11:13 +0000

    Added Api functions
    Added function to refresh temperature HTML

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function displayTemperatureFunction(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "6778eb30a3f8f38t710a50259f4eoc6f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperatureFunction);
+}
+
 function citySearchFunction(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input-search-text");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", citySearchFunction);
+
+searchCity("London");
