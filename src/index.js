336bd343b119a45b9ca620c0d2704195b6d1e015
Ma-Lucy <161073044+Ma-Lucy@users.noreply.github.com>
2024-03-20 12:24:10 +0000

    Added date formatting function

--- src/index.js
+++ src/index.js
@@ -5,14 +5,35 @@ function displayTemperatureFunction(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dayTimeElement = document.querySelector("#day-and-time");
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
 
+  dayTimeElement.innerHTML = dayFunction(date);
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+}
 
-  console.log(response.data);
+function dayFunction(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
